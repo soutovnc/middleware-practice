9912358e8844553e3ee57223774a41c7323bc44f
Vinicius <viniciusdesouto@gmail.com>
2021-09-21 01:27:26 -0300

    missing one more

--- src/index.js
+++ src/index.js
@@ -24,7 +24,13 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro === false && user.todos.length >= 10) {
+    return response.status(403).json({ error: 'Limit exhausted'})
+  }
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
